d3526d2dd3c76d20d2e0708e03e7b8bb42ab98bf
Oksana Glushchenko <glushchenko.oksana.it@gmail.com>
2017-08-31 17:13:55 +0300

    correctly building jar files

--- scripts/runVERA.sh
+++ scripts/runVERA.sh
@@ -4,8 +4,8 @@ iterNum="$1"
 
 if [ -z "$4" ]
 	then
-		java -jar ../target/microbiomeres-0.1.jar $iterNum $(realpath "$2") $(realpath "$3")
+		java -jar ../target/microbiomeres-0.1-jar-with-dependencies.jar $iterNum $(realpath "$2") $(realpath "$3")
 	else
-		java -jar ../target/microbiomeres-0.1.jar $iterNum $(realpath "$2") $(realpath "$3") $(realpath "$4")
+		java -jar ../target/microbiomeres-0.1-jar-with-dependencies.jar $iterNum $(realpath "$2") $(realpath "$3") $(realpath "$4")
 fi
 
