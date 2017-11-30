ba1d023bb005d2bc8f0a3feeb5d6cacc3c36f32f
Oksana Glushchenko <glushchenko.oksana.it@gmail.com>
2017-11-30 12:22:37 +0300

    removed unused files

--- scripts/estimationReportRUN.sh
+++ scripts/estimationReportRUN.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# bash estimationReportRUN.sh ../out/simulations/Jun_23_time_17_07/ ../resources/config.properties ..out/estimationOfOOI
+# bash estimationReportRUN.sh -i ../out/simulations/mruns -c  ../resources/config.properties
 
+while getopts i:c: option
+do
+ case "${option}"
+ in
+ i) inpDir=$(readlink --canonicalize "${OPTARG}");;
+ c) confile=$(readlink --canonicalize "${OPTARG}");;
+ esac
+done
 
-abTabFolder=$(readlink --canonicalize "$1")
+abund=$inpDir"/abundTables"
+outPath=$inpDir"/OOIestimation"
 
-echo "read $abTabFolder"
-#bufVar=$(date +"%b")"_"$(date +"%e") #"_time_"$(date +"%H")"_"$(date +"%M")
-
-#echo "try read $($(dirname $0)/$3)"
-mkdir -p "$(dirname $0)/$3"
-outPath=$(readlink --canonicalize "$3")
 echo "Created folder $outPath"
 mkdir -p $outPath"/tmp"
 
-cpTab=$(readlink --canonicalize "$2")
+cpN=$(readlink --canonicalize "$(dirname $0)/../resources/cp_notes")
+#echo "cp notes doc $cpN"
 
-cpN="../resources/cp_notes"
-
-Rscript --vanilla "$(dirname $0)/optim_rate_estimation.R" $abTabFolder $cpTab $outPath $cpN >> "$(dirname $0)/../out/log/log.est"
+Rscript --vanilla $(readlink --canonicalize "$(dirname $0)/optim_rate_estimation.R") $abund $confile $outPath $cpN >> $(readlink --canonicalize "$(dirname $0)/../out/log/log.est")
 
 rm -rf $outPath"/tmp"
 
